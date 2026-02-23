a297bbbdb37cd1342edf38981c91af62cb08ce62
chandan thakur <chandan7073251686@gmail.com>
2026-02-24 01:39:44 +0530

    same code update

--- 02Backend_M/userModel.js
+++ 02Backend_M/userModel.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 mongoose.connect(`mongodb://localhost:27017/test`);
 
 
-const create = mongoose.Schema({
+const userSchema = mongoose.Schema({
     name: String,
     username: String,
     email: String
 });
 
-module.exports =  mongoose.model("user", create);
+module.exports =  mongoose.model("user", userSchema);
